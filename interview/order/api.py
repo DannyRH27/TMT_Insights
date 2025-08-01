4d3c8192bd31a9c39ba3051e19616eb1d84c8f10
Danny Huang <dannyhuang@Dannys-MacBook-Pro.local>
2025-08-01 11:20:06 -0700

    add error-logs

--- interview/order/api.py
+++ interview/order/api.py
@@ -1,12 +1,18 @@
 from interview.order.models import Order
 from interview.order.serializers import OrderTagSerializer
 
+from rest_framework.response import Response
+from rest_framework.request import Request
+
 # if order doesn't exist, don't grab tags and don't instantiate serializer
 
-def retrieve_tags_by_order(order: Order) ->list[dict]:
+def retrieve_tags_by_order(order: Order) -> Response:
     if not Order.objects.filter(id=order.id).exists():
-        return []
+        return Response(f"Order with Order ID {order.id} does not exist", status=404)
 
     tags = Order.tags.all()
 
-    return OrderTagSerializer(tags, many=True).data
+    if not tags:
+        return Response(f"No tags associated with Order ID {order.id}", status=404)
+
+    return Response(OrderTagSerializer(tags, many=True).data, status=200)
